4d5069af88e92a0d275aa6519864ecdb9617b5a5
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-20 11:32:49 +0200

    adding comments

--- src/Components/CategoryTile/CategoryTile.tsx
+++ src/Components/CategoryTile/CategoryTile.tsx
@@ -6,22 +6,16 @@ import { Category } from '../../helpers/interfaces';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-// CategoryTile:
-// 1. Stwórz interface z propsami (przyjmuje stringa)
-// 2. Stwórz stan image (string)
-// 3. useEffect: fetch z `fakestoreapi.com/products/category/${category}?limit=1`, z danych które przyjdą do stanu wchodzi własność image, DA: []
-
-// JSX:
-// Card
-// Typography z textem {category}
-// wyświeltnie obrazka wg stanu image (Box, component='img' src='')
-
 const CategoryTile: React.FC<Category> = ({ category }) => {
+	//state for setting category img
 	const [image, setImage] = React.useState<string[] | undefined>();
+
+	//fetching category img from API
 	React.useEffect(() => {
 		axios
 			.get(`https://fakestoreapi.com/products/category/${category}?limit=1`)
 			.then((response) => {
+				//setting img url to state variable image
 				setImage(response.data[0].image);
 			})
 			.catch((e) => {
@@ -36,6 +30,7 @@ const CategoryTile: React.FC<Category> = ({ category }) => {
 				variant="inherit"
 				component="div"
 				sx={{ mb: '1rem' }}>
+				{/* displaying category title */}
 				{category.toUpperCase()}
 			</Typography>
 			<Box component="img" src={image} sx={{ maxWidth: '60%' }}></Box>
