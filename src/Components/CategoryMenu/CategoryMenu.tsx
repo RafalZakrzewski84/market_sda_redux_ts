b9c4986e7c7ad38a5c3e68703c4436bbac35ba6f
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-20 11:07:17 +0200

    styling paper and removing comments

--- src/Components/CategoryMenu/CategoryMenu.tsx
+++ src/Components/CategoryMenu/CategoryMenu.tsx
@@ -7,21 +7,6 @@ import Paper from '@mui/material/Paper';
 import { CategoryMenuProps } from '../../helpers/interfaces';
 import CategoryTile from '../CategoryTile/CategoryTile';
 
-// JSX:
-// Paper
-// {categories.map}:
-// CategoryTile (przez props podajemy daną kategorie)
-
-// CategoryTile:
-// 1. Stwórz interface z propsami (przyjmuje stringa)
-// 2. Stwórz stan image (string)
-// 3. useEffect: fetch z `fakestoreapi.com/products/category/${category}?limit=1`, z danych które przyjdą do stanu wchodzi własność image, DA: []
-
-// JSX:
-// Card
-// Typography z textem {category}
-// wyświeltnie obrazka wg stanu image (Box, component='img' src='')
-
 const CategoryMenu: React.FC<CategoryMenuProps> = ({ categories }) => {
 	console.log(categories);
 	return (
@@ -33,7 +18,9 @@ const CategoryMenu: React.FC<CategoryMenuProps> = ({ categories }) => {
 			}}>
 			{categories.map((category) => {
 				return (
-					<Paper elevation={3} sx={{ mx: '1rem', p: '1rem 2rem' }}>
+					<Paper
+						elevation={3}
+						sx={{ m: '1rem', p: '1rem 2rem', width: '250px' }}>
 						{category}
 						<CategoryTile category={category} />
 					</Paper>
