0a74ba418814ee7448a4933734c713d806824409
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-20 20:05:27 +0200

    adding interfaces for product

--- src/helpers/interfaces.tsx
+++ src/helpers/interfaces.tsx
@@ -7,3 +7,26 @@ export interface CategoryMenuProps {
 export interface CategoryTileProps {
 	category: string;
 }
+
+// interface Product
+// interface Rating
+
+interface Rating {
+	rate: number;
+	count: number;
+}
+
+//can we export Product?
+interface Product {
+	id: number;
+	title: string;
+	price: number;
+	description: string;
+	category: string;
+	image: string;
+	rating: Rating;
+}
+
+export interface ProductProps {
+	product: Product;
+}
