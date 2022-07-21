389bcfa699a35435112dfd8a6ac8ecb372bf04ad
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 19:46:05 +0200

    adding correct routes in app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Components/HomePage/HomePage';
 import ProductPage from './Components/ProductPage/ProductPage';
+import Cart from './Components/Cart/Cart';
+import LoginPage from './Components/LoginPage/LoginPage';
+import Register from './Components/Register/Register';
+import UserAccount from './Components/UserAccount/UserAccount';
 
 function App() {
 	return (
@@ -14,18 +18,28 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<HomePage />} />
-					{/* <Route path="/electronics" element={<Electronic />} />
-					<Route path="/jewelry" element={<Jewelry />} />
-					<Route path="/mensclothing" element={<Mensclothing />} />
-					<Route path="/womensclothing" element={<Womensclothing />} />
+					<Route
+						path="/electronics"
+						element={<ProductPage category={'electronics'} />}
+					/>
+					<Route
+						path="/jewelry"
+						element={<ProductPage category={'jewelery'} />}
+					/>
+					<Route
+						path="/mensclothing"
+						element={<ProductPage category={"men's clothing"} />}
+					/>
+					<Route
+						path="/womensclothing"
+						element={<ProductPage category={"women's clothing"} />}
+					/>
 					<Route path="/cart" element={<Cart />} />
 					<Route path="/login" element={<LoginPage />} />
 					<Route path="/register" element={<Register />} />
-					<Route path="/useraccount" element={<UserAccount />} /> */}
+					<Route path="/useraccount" element={<UserAccount />} />
 				</Routes>
 			</BrowserRouter>
-			<ProductPage category={'electronics'} />
-			{/* <HomePage /> */}
 		</div>
 	);
 }
