91c99ac359989a995845a58df3b7c4e7aeeab88f
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 20:08:23 +0200

    adding links in category tiles

--- src/Components/CategoryMenu/CategoryMenu.tsx
+++ src/Components/CategoryMenu/CategoryMenu.tsx
@@ -3,12 +3,19 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import { Link } from 'react-router-dom';
 
 import { CategoryMenuProps } from '../../helpers/interfaces';
 import CategoryTile from '../CategoryTile/CategoryTile';
 
 const CategoryMenu: React.FC<CategoryMenuProps> = ({ categories }) => {
-	console.log(categories);
+	console.log('CategoryMenu', categories);
+	const links = [
+		'/electronics',
+		'/jewelry',
+		'/mensclothing',
+		'/womensclothing',
+	];
 	return (
 		<Box
 			sx={{
@@ -22,7 +29,9 @@ const CategoryMenu: React.FC<CategoryMenuProps> = ({ categories }) => {
 						key={idx}
 						elevation={3}
 						sx={{ m: '1rem', p: '1rem 2rem', width: '250px' }}>
-						<CategoryTile category={category} />
+						<Link key={links[idx]} to={links[idx]}>
+							<CategoryTile category={category} />
+						</Link>
 					</Paper>
 				);
 			})}
