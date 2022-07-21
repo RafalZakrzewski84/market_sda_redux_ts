4c96c47b04f125a2b0a9d433720b4c123847cb6e
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-21 18:57:43 +0200

    adding routing components to app and products page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 /** @format */
 
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import HomePage from './Components/HomePage/HomePage';
+import ProductPage from './Components/ProductPage/ProductPage';
 
 function App() {
 	return (
 		<div className="App">
-			<Navbar />
-			<HomePage />
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					{/* <Route path="/electronics" element={<Electronic />} />
+					<Route path="/jewelry" element={<Jewelry />} />
+					<Route path="/mensclothing" element={<Mensclothing />} />
+					<Route path="/womensclothing" element={<Womensclothing />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/useraccount" element={<UserAccount />} /> */}
+				</Routes>
+			</BrowserRouter>
+			<ProductPage category={'electronics'} />
+			{/* <HomePage /> */}
 		</div>
 	);
 }
