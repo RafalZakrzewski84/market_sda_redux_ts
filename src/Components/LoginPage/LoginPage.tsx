7cba4956d66adbe4cc850b1210163495399f30f5
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-23 13:24:43 +0200

    structuring login page adding login form comp. and link to register

--- src/Components/LoginPage/LoginPage.tsx
+++ src/Components/LoginPage/LoginPage.tsx
@@ -1,13 +1,36 @@
 /** @format */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+
+import LoginFrom from '../LoginForm/LoginForm';
 import Register from '../Register/Register';
 
 function LoginPage() {
 	return (
-		<>
-			<Register />
-		</>
+		<Container align="center" maxWidth="sm">
+			<Card sx={{ minWidth: 275, my: 4 }}>
+				<LoginFrom />
+			</Card>
+			<Card sx={{ minWidth: 275, my: 2 }}>
+				<Typography
+					variant="h2"
+					component="h2"
+					sx={{ fontSize: '1.2rem', my: 2 }}>
+					Don't have an account yet? Register now!
+				</Typography>
+				<Link to="/register" style={{ textDecoration: 'none' }}>
+					<Button variant="contained" sx={{ mb: 2 }}>
+						Register
+					</Button>
+				</Link>
+			</Card>
+		</Container>
 	);
 }
 
