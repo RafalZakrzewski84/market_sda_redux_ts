0fb031c32736d9660ba00914f22507572c35df39
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-25 20:57:51 +0200

    importing reducer for productCart

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { authReducer } from './authReducers';
+import { productCartReducer } from "./productCartReducers";
 
 
 //combining reducers to one pack
 export const rootReducer = combineReducers({
     authState: authReducer,
+    productsInCartState: productCartReducer,
 })
