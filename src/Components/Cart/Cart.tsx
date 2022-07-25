3dcf90ed7dcff5b7d7fd5c0471e2ea55a8748f59
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-25 21:13:26 +0200

    now product come to cart from redux state

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -12,33 +12,44 @@ import { Product } from '../../helpers/interfaces';
 import { getDatabase, ref, set } from 'firebase/database';
 import firebase from '../../helpers/firebaseConfig';
 
+import { useSelector } from 'react-redux';
+
+interface ProductsInCartState {
+	productsInCartState: Product[];
+}
+
+interface ProductsState {
+	productsInCartState: ProductsInCartState;
+}
+
 function Cart() {
 	//state fro products from API and their total price
 	const [products, setProducts] = React.useState<Product[]>();
 	const [totPrice, setTotPrice] = React.useState<number>(0);
 
-	React.useEffect(() => {
-		axios
-			.get('https://fakestoreapi.com/products?limit=5')
-			.then((res) => {
-				// console.log('Cart', res.data);
-				setProducts(res.data);
+	const productsInCart = useSelector(
+		(state: ProductsState) => state.productsInCartState.productsInCartState
+	);
 
-				//sum of prices of products used to set state
-				const prices = res.data
-					.map((item: Product) => item.price)
-					.reduce((a: number, b: number) => {
-						return a + b;
-					});
-				setTotPrice(prices);
-			})
-			.catch((e) => console.log(e));
-	}, []);
+	console.log(productsInCart);
+	React.useEffect(() => {
+		if (productsInCart.length > 0) {
+			const prices = productsInCart
+				.map((item: Product) => item.price)
+				.reduce((a: number, b: number) => {
+					return a + b;
+				});
+			setTotPrice(prices);
+		} else {
+			setTotPrice(0);
+		}
+	}, [productsInCart]);
 
 	//adding content of cart to db
 	const placeOrder = () => {
 		//fn to sending products to db in ref() is URL
 		//IMPORTANT add somehow order number
+		//change this function to Updating or deleting data from FB data
 		function placeOrderInDB(uid: string | null, products: Product[]) {
 			const db = getDatabase();
 			set(ref(db, 'usersOrders/' + uid), products);
@@ -84,8 +95,8 @@ function Cart() {
 					justifyContent: 'center',
 					flexWrap: 'wrap',
 				}}>
-				{products &&
-					products.map((product: Product, idx: number) => (
+				{productsInCart &&
+					productsInCart.map((product: Product, idx: number) => (
 						<CartProductTile key={idx} product={product} />
 					))}
 			</Box>
