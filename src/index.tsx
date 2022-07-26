5f75446ecc59c1b3617cb5be6656479267a7d462
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-26 22:16:07 +0200

    changing palace of provider component

--- src/index.tsx
+++ src/index.tsx
@@ -16,9 +16,9 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
+	<Provider store={store}>
+		<React.StrictMode>
 			<App />
-		</Provider>
-	</React.StrictMode>
+		</React.StrictMode>
+	</Provider>
 );
