9323b5ab2f49c33ee2794bd3d3df2fd34edddbac
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-26 19:12:31 +0200

    adding product state to reducers

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import { authReducer } from './authReducers';
 import { productCartReducer } from "./productCartReducers";
-
+import { productReducer } from "./productsReducer";
 
 //combining reducers to one pack
 export const rootReducer = combineReducers({
     authState: authReducer,
     productsInCartState: productCartReducer,
+    productsState: productReducer,
 })
