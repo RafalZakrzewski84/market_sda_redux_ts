ddc4df75ab33cd32092ee249f0a4d41f3a4f2f5f
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-26 22:15:42 +0200

    adding action for fetching category img

--- src/redux/actions/productsAction.ts
+++ src/redux/actions/productsAction.ts
@@ -1,18 +1,38 @@
 import { ActionTypes } from "../constants/actionTypes";
 import { ProductsInitialState } from '../../helpers/interfaces'
 import axios from "axios";
+
+//two imports below are interfaces
 import { ThunkAction } from "redux-thunk";
 import { AnyAction } from 'redux';
 
-//action for fetching 10 products from API
+//action for fetching 10 products from API - action dispatched in Bestsellers
 export const fetchProducts = (quantity: number): ThunkAction<void, ProductsInitialState, number, AnyAction> => {
     //fetchProducts function will return dispatching function witch data from API
     return async (dispatch) => {
         const response = await axios.get(`https://fakestoreapi.com/products?limit=${quantity}`);
         dispatch({
-            //this is action.payload in productReducer
+            //this is action.payload in productReducer for fetchedProducts
             type: ActionTypes.FETCH_PRODUCTS,
             payload: response.data
+        })
+    }
+}
+
+//action for fetching category images - action dispatched in CategoryTile
+export const fetchCategoryImg = (categories: string[]): ThunkAction<void, ProductsInitialState, string, AnyAction> => {
+    console.log('fetchCategoryImg', categories)
+    return async (dispatch) => {
+        const images = []
+        for (let category of categories) {
+            const response = await axios.get(`https://fakestoreapi.com/products/category/${category}?limit=1`);
+            images.push(response.data[0].image)
+        }
+
+        dispatch({
+            //this is action.payload in productReducer for fetchedImages
+            type: ActionTypes.FETCH_CATEGORY_IMAGES,
+            payload: images,
         })
     }
 }
