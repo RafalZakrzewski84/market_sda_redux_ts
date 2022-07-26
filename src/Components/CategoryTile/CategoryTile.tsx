5e319118d362204ef4bca7a8f3a693ca09befa1f
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-26 22:17:45 +0200

    removing fetching from components and props img

--- src/Components/CategoryTile/CategoryTile.tsx
+++ src/Components/CategoryTile/CategoryTile.tsx
@@ -1,28 +1,11 @@
 /** @format */
 
 import * as React from 'react';
-import axios from 'axios';
 import { CategoryTileProps } from '../../helpers/interfaces';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-const CategoryTile: React.FC<CategoryTileProps> = ({ category }) => {
-	//state for setting category img
-	const [image, setImage] = React.useState<string>();
-
-	//fetching category img from API
-	React.useEffect(() => {
-		axios
-			.get(`https://fakestoreapi.com/products/category/${category}?limit=1`)
-			.then((response) => {
-				//setting img url to state variable image
-				setImage(response.data[0].image);
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	}, []);
-
+const CategoryTile: React.FC<CategoryTileProps> = ({ category, image }) => {
 	return (
 		<>
 			<Typography
