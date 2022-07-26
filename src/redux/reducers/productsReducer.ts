4624659890cc92b1f69848e827de4950043543d2
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-26 22:12:57 +0200

    adding fetched image state

--- src/redux/reducers/productsReducer.ts
+++ src/redux/reducers/productsReducer.ts
@@ -4,14 +4,23 @@ import { ActionTypes } from "../constants/actionTypes";
 //initial state for fetched products
 const initialState = {
     fetchedProducts: [],
+    fetchedImages: [],
 };
 
+//reducer inform which state update
 export const productReducer = (state = initialState, action: AnyAction) => {
     switch (action.type) {
         case ActionTypes.FETCH_PRODUCTS:
             return {
                 //returning of new state using dispatch action in bestsellers component
-                fetchedProducts: action.payload
+                fetchedProducts: action.payload,
+                fetchedImages: [...state.fetchedImages]
+            }
+        case ActionTypes.FETCH_CATEGORY_IMAGES:
+            return {
+                //state updated by category img fetched by action dispatched in CategoryTile
+                fetchedProducts: [...state.fetchedProducts],
+                fetchedImages: action.payload
             }
 
         default:
